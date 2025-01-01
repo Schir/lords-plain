36f7bb0a7e8b0bd2b6f10298d0f0b7af9e1b0767
Schir <schirsa@gmail.com>
2025-01-01 07:35:37 -0700

    starting to work on making the items lerp

--- item.gd
+++ item.gd
@@ -4,11 +4,11 @@ extends Node3D
 
 @export var weight : float
 var is_rotating : bool = false
+var initialPosition
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	initialPosition = position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
